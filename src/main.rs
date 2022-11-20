f9e94c391a5ca32cff3f961f7effcb97fb4c788e
Ollivier Robert <roberto@keltia.net>
2022-11-20 23:56:34 +0100

    Move code into their own files.  Keep minimalistic main(à.

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,26 @@
+mod auth;
 mod config;
-mod version;
 mod macros;
+mod version;
 
 #[cfg(not(unix))]
-compile_error!("Not available outside of the Unix World");
+//compile_error!("Not available outside of the Unix World");
+use std::path::PathBuf;
 
 use anyhow::Result;
-use csv::ReaderBuilder;
 
-use crate::config::{CONFIG_FILE, User};
+use crate::config::{User, CONFIG_FILE, Config, BASEDIR};
 use crate::version::{CALIFE_NAME, CALIFE_VERSION};
 
 fn main() -> Result<()> {
     println!("Hello, world! {} = {}", CALIFE_NAME, CALIFE_VERSION);
+    let def: PathBuf = makepath!(BASEDIR, CONFIG_FILE);
+    println!("Default config in {:?}", def);
 
-    let mut rdr = ReaderBuilder::new()
-        .flexible(true)
-        .delimiter(b':')
-        .from_path(CONFIG_FILE)
-        .unwrap_or_else(|e| panic!("Unknown file {}: {}", CONFIG_FILE, e));
-
-    let _users: Vec<_> = rdr
-        .records()
-        .map(|user| {
-            let user = user.unwrap();
-
-            let user: User = user.deserialize(None).unwrap();
-            user
-        }).collect();
+    // Temp for devlopment
+    //
+    let users = Config::load(PathBuf::from("testdata/calife.auth"))?;
+    dbg!(&users);
 
     Ok(())
 }
