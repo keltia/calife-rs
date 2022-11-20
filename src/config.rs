76176216dc7efa0250b1f1931edc8f66114873a4
Ollivier Robert <roberto@keltia.net>
2022-11-20 23:50:06 +0100

    Move stuff from main.rs.  Add reading configuration file.

--- src/config.rs
+++ src/config.rs
@@ -19,11 +19,37 @@ pub struct User {
     pub users: Option<Vec<Become>>,
 }
 
-#[derive(Debug, Deserialize)]
+impl Default for User {
+    /// Invalid user role by default
+    ///
+    fn default() -> Self {
+        User {
+            name: "NONE".to_owned(),
+            shell: None,
+            who: None,
+        }
+    }
+}
+
+/// Enum for the users/groups the user will have rights on
+///
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq)]
 #[serde(untagged)]
 pub enum Become {
+    /// Plain su-like role
+    Root,
+    /// A specific user
     User(String),
+    /// A group
     Group(String),
+}
+
+impl Default for Become {
+    /// Most basic role
+    ///
+    fn default() -> Self {
+        Become::Root
+    }
 }
 
 impl From<&str> for Become {
@@ -36,6 +62,36 @@ impl From<&str> for Become {
     }
 }
 
+impl Become {
+    /// Generate a list of roles from a vector of entries from the file
+    ///
+    pub fn from_vec(v: Vec<&str>) -> Vec<Self> {
+        v.iter().map(|&e| Become::from(e)).collect()
+    }
+}
+
+/// Holds the path for a shell
+///
+#[derive(Clone, Deserialize, Debug)]
+pub struct Shell(String);
+
+impl Default for Shell {
+    /// Reasonable default
+    ///
+    fn default() -> Self {
+        Shell("/bin/zsh".to_string())
+    }
+}
+
+impl From<&str> for Shell {
+    fn from(s: &str) -> Self {
+        match s {
+            "" => Shell::default(),
+            _ => Shell(s.to_owned())
+        }
+    }
+}
+
 /// Returns the path of the default config file
 ///
 pub fn default_file() -> PathBuf {
@@ -44,3 +100,57 @@ pub fn default_file() -> PathBuf {
     def
 }
 
+/// Empty struct used as "namespace"
+///
+pub struct Config;
+
+impl Config {
+    /// Basic reader for the original configuration file
+    ///
+    pub fn load(fname: PathBuf) -> Result<Auth> {
+        let lines = fs::read_to_string(fname)?;
+        let content = lines.lines();
+        let users = content
+            .map(|line| {
+                let fields: Vec<_> = line.split(":").collect();
+                let n = fields[0];
+
+                // Better way to do this maybe?
+                //
+                match fields.len() {
+                    1 => User {
+                        name: n.to_owned(),
+                        shell: Some(Shell::default()),
+                        who: Some(vec![Become::Root]),
+                    },
+                    2 => User {
+                        name: n.to_owned(),
+                        shell: Some(Shell::from(fields[1])),
+                        who: Some(vec![Become::Root]),
+                    },
+                    3 => {
+                        let users: Vec<&str> = fields[2].split(",").collect();
+                        User {
+                            name: n.to_owned(),
+                            shell: Some(Shell::from(fields[1])),
+                            who: Some(Become::from_vec(users)),
+                        }
+                    }
+                    _ => User {
+                        name: "INVALID".to_owned(),
+                        shell: None,
+                        who: None,
+                    },
+                }
+            })
+            // Filter out invalid entries
+            //
+            .filter(|u| u.name != "INVALID")
+            .map(|u| (u.name.to_owned(), u))
+            // Create a HashMap of all entries
+            //
+            .into_group_map();
+
+        Ok(Auth::new(users))
+    }
+}
