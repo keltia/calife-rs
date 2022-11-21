7eca216f32c0e7a36148eecf9584e65c1d29f531
Ollivier Robert <roberto@keltia.net>
2022-11-21 15:51:46 +0100

    Documentation.

--- src/auth.rs
+++ src/auth.rs
@@ -8,14 +8,20 @@ use crate::subr::{Become, User};
 pub struct Auth(HashMap<String, Vec<User>>);
 
 impl Auth {
+    /// Instantiate a new authentication database
+    ///
     pub fn new(a: HashMap<String, Vec<User>>) -> Self {
         Auth(a)
     }
 
+    /// Check whether a given user exist
+    ///
     pub fn exist(&self, user: &str) -> bool {
         self.0.contains_key(user)
     }
 
+    /// Check whether a given user is authorized to become someone else
+    ///
     pub fn authorized(&self, _who: Become) -> bool {
         false
     }
