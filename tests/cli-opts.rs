edeca90258207a2c45e1a0abb49c71dfeab0dc31
Ollivier Robert <roberto@keltia.net>
2022-11-21 15:26:01 +0100

    Add basic CLI tests.

--- tests/cli-opts.rs
+++ tests/cli-opts.rs
@@ -1,2 +1,17 @@
 use assert_cmd::Command;
 
+const BIN: &str = "calife";
+
+#[test]
+fn test_help() {
+    let mut cmd = Command::cargo_bin(BIN).unwrap();
+    cmd.arg("-h").assert().success();
+}
+
+#[test]
+fn test_version() {
+    let mut cmd = Command::cargo_bin(BIN).unwrap();
+    cmd.arg("-V").assert().success();
+}
+
+
